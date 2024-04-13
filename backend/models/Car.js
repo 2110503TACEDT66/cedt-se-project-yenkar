81a79f196a9ffbbc970a722d61bd14e7fc5ad994
Nithinan_ <nwattanasiri47@gmail.com>
2024-04-13 13:31:36 +0700

    Added Car Model (But not include Image field)

---
+++ backend/models/Car.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const CarProvider = require("./CarProvider");
+
+const carSchema = new mongoose.Schema({
+    carProvider: { // carProvider is Reference to which CarProvider's _id
+        type: mongoose.Schema.ObjectId,
+        ref: 'CarProvider',
+        require: [true, "Please provide car provider"],
+    },
+    brand: { 
+        type: String,
+        required: [true, "Please provide car's brand"],
+    },
+    model: {
+        type: String,
+        required: [true, "Please provide car's model"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Please provide car's rental price"],
+        min: [0, "Price can't below than 0"],
+    },
+    // image: { ?????????????????????
+    //     type: String,
+        
+    // }   
+});
+
+module.exports = mongoose.model("Car", carSchema);
