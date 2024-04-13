79e26c1bb83015fb3875e1c33d57110467d43b1c
Nithinan_ <nwattanasiri47@gmail.com>
2024-04-13 13:48:55 +0700

    Added Carprovider route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const {register, login, getMe, logout ,deleteUser} = require('../controllers/auth');
+const {register, login, getMe, logout ,deleteUser} = require('../controllers/auth/user');
+const { carProviderRegister, carProviderLogin, getMeCarProvider } = require("../controllers/auth/carProvider");
 
 const router = express.Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
+///////// For Customer /////
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/logout', logout);
 router.delete('/deleteuser/:id',protect,authorize('admin'),deleteUser);
+///////////////////////////////////
 
+///////// For Car Provider /////
+router.post('/register/carProvider', carProviderRegister);
+router.post('/login/carProvider', carProviderLogin);
+router.get('/me/carProvider', protect, getMeCarProvider);
+router.get('/logout', logout);
+
+////////////////////////////////
 
 module.exports = router;
