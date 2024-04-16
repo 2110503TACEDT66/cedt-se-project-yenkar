10c46173695e6315a7df73b9628766d03c2a5b5b
Nithinan_ <nwattanasiri47@gmail.com>
2024-04-16 17:10:17 +0700

    Add deleteCar from backend to libs

---
+++ frontend/src/libs/deleteCar.tsx
@@ -0,0 +1,16 @@
+export default async function deleteCar(id: string, token?: string) {
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/api/v1/cars/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to delete car");
+    }
+    return response.json();
+  }
+  
