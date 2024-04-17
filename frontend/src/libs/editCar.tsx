4a815b72ac734aca693bbf779e3642c765ce1b55
kphotchara <kphotchara@gmail.com>
2024-04-17 13:55:33 +0700

    editCar libs in Frontend

---
+++ frontend/src/libs/editCar.tsx
@@ -0,0 +1,19 @@
+export default async function editCar(token:string,cid:string,brand?:string,model?:string,price?:number,src?:string) {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/cars/${cid}`,{
+        method: "PUT",
+        headers: {
+            "Content-Type":"application/json",
+            authorization:`Bearer ${token}`
+        },
+        body: JSON.stringify({
+            brand : brand,
+            model : model,
+            price : price,
+            src : src
+        }),
+    })
+    if(!response.ok){
+        throw new Error("failed to add booking") 
+    }
+    return await response.json()
+}
