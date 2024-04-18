b0f2335a6fbbd50616b535d7f6c1d29145c0ca44
Nithinan_ <nwattanasiri47@gmail.com>
2024-04-18 14:42:39 +0700

    Add providerRegister to libs

--- frontend/src/libs/providerRegister.tsx
+++ frontend/src/libs/providerRegister.tsx
@@ -3,7 +3,7 @@ export default async function providerRegister(name:string, telephone:string, ad
     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/register/carProvider`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"  
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({
             email,
