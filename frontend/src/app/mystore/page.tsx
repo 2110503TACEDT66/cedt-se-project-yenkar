fd5723dfb1c62a67dbf1d37e31c2d61fdc5509ea
Tata11jr <W.pannawich@gmail.com>
2024-04-18 15:08:51 +0700

    fix showcar in explore

--- frontend/src/app/mystore/page.tsx
+++ frontend/src/app/mystore/page.tsx
@@ -108,7 +108,7 @@ const page = ({ params }: { params: { id: string } }) => {
                               <Image src={carItem?.src!} alt="carpic" width={300} height={100} className="w-full h-[70%] rounded-t-2xl"/>
                               <div className="mt-5 ml-5 text-xl font-bold">{carItem.model}</div>
                               <div className="mt-2 ml-5 text-m">{carItem.brand}</div>
-                              <div className="mt-2 ml-5">{`฿${carItem.price}`}</div>
+                              <div className="mt-2 ml-5">{`฿ ${carItem.price}`}</div>
                           </div>
                         </Link>
                     ):""}
