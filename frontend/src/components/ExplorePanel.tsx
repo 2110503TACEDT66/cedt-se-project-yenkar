4d303510b516f018e99e49af286a24e52edb236c
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-04-27 22:23:08 +0700

    Add Skeleton component to ExplorePanel and ProviderPanel components

--- frontend/src/components/ExplorePanel.tsx
+++ frontend/src/components/ExplorePanel.tsx
@@ -3,12 +3,15 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import ExploreCard from "./ExploreCard";
+import { Skeleton } from "./ui/skeleton";
 
 const ExplorePanel = ({ carJson }: { carJson: Promise<CarJson> }) => {
   const [carData, setCardData] = useState<CarJson>();
+  const [isLoading, setIsLoading] = useState(true);
   const resolve = () => {
     const carJsonReady = carJson.then((res) => {
       setCardData(res);
+      setIsLoading(false);
     });
   };
 
@@ -18,23 +21,36 @@ const ExplorePanel = ({ carJson }: { carJson: Promise<CarJson> }) => {
   //console.log(carJsonReady);
   return (
     <div className="w-[93%] h-2 flex flex-row flex-wrap drop-shadow-md">
-      {carData?.data.map((carItem: CarItem) => (
-        <div className="w-[24%] h-[35rem] m-2 rounded-lg relative hover:scale-[102%] transition duration-200 ease-in-out active:scale-100 flex-grow ">
-          <Link
-            href={`/reserve?pid=${carItem.carProvider._id}&cid=${carItem._id}`}
-          >
-            <ExploreCard
-              key={carItem._id}
-              _id={carItem._id}
-              model={carItem.model}
-              brand={carItem.brand}
-              carProvider={carItem.carProvider}
-              price={carItem.price}
-              src={carItem.src}
-            />
-          </Link>
-        </div>
-      ))}
+      {isLoading ? (
+        <>
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+        </>
+      ) : (
+        carData?.data.map((carItem: CarItem) => (
+          <div className="w-[24%] h-[35rem] m-2 rounded-lg relative hover:scale-[102%] transition duration-200 ease-in-out active:scale-100 flex-grow ">
+            <Link
+              href={`/reserve?pid=${carItem.carProvider._id}&cid=${carItem._id}`}
+            >
+              <ExploreCard
+                key={carItem._id}
+                _id={carItem._id}
+                model={carItem.model}
+                brand={carItem.brand}
+                carProvider={carItem.carProvider}
+                price={carItem.price}
+                src={carItem.src}
+              />
+            </Link>
+          </div>
+        ))
+      )}
 
       {carData?.data?.length! % 4 === 0
         ? null
--- frontend/src/components/ProviderPanel.tsx
+++ frontend/src/components/ProviderPanel.tsx
@@ -4,15 +4,18 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import ExploreCard from "./ExploreCard";
 import ProviderCard from "./ProviderCard";
+import { Skeleton } from "./ui/skeleton";
 const ProviderPanel = ({
   providerJson,
 }: {
   providerJson: Promise<CarProviderJson>;
 }) => {
   const [providerData, setProviderData] = useState<CarProviderJson>();
+  const [isLoading, setIsLoading] = useState(true);
   const resolve = () => {
     const carJsonReady = providerJson.then((res) => {
       setProviderData(res);
+      setIsLoading(false);
     });
   };
 
@@ -22,19 +25,33 @@ const ProviderPanel = ({
   //console.log(carJsonReady);
   return (
     <div className="w-[93%] h-2 flex flex-row flex-wrap">
-      {providerData?.data.map((providerItem: CarProvider) => (
-        <div className="w-[24%] h-[35rem] m-2 rounded-lg relative hover:scale-[102%] transition duration-200 ease-in-out active:scale-100 flex-grow">
-          <Link href={`/explore/provider/${providerItem._id}`}>
-            <ProviderCard
-              _id={providerItem._id}
-              name={providerItem.name}
-              address={providerItem.address}
-              telephone={providerItem.telephone}
-              src={providerItem.src}
-            />
-          </Link>
-        </div>
-      ))}
+      {isLoading ? (
+        <>
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+          <Skeleton className="w-[24%] h-[35rem] m-2 rounded-lg bg-black" />
+        </>
+      ) : (
+        providerData?.data.map((providerItem: CarProvider) => (
+          <div className="w-[24%] h-[35rem] m-2 rounded-lg relative hover:scale-[102%] transition duration-200 ease-in-out active:scale-100 flex-grow">
+            <Link href={`/explore/provider/${providerItem._id}`}>
+              <ProviderCard
+                _id={providerItem._id}
+                name={providerItem.name}
+                address={providerItem.address}
+                telephone={providerItem.telephone}
+                src={providerItem.src}
+              />
+            </Link>
+          </div>
+        ))
+      )}
+
       {providerData?.data?.length! % 4 === 0
         ? null
         : Array.from({ length: 4 - (providerData?.data?.length! % 4) }).map(
