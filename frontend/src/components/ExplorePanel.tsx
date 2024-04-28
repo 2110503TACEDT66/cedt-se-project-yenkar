f38dab07d1d85ef5cd42f20a19b0e5fb17cba4a0
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2024-04-28 22:05:28 +0700

    Refactor ExplorePanel component to handle empty car list

--- frontend/src/components/ExplorePanel.tsx
+++ frontend/src/components/ExplorePanel.tsx
@@ -35,7 +35,7 @@ const ExplorePanel = ({ carJson }: { carJson: Promise<CarJson> }) => {
   //console.log(carJsonReady);
   return (
     <>
-      {myCar?.length! > 0 ? (
+      {myCar?.length! > 0 && session?.user.role === "carProvider" ? (
         <>
           <h1 className="self-start text-white ml-[4vw] font-kiona text-2xl font-medium">
             {" "}
@@ -71,42 +71,47 @@ const ExplorePanel = ({ carJson }: { carJson: Promise<CarJson> }) => {
           </h1>
         </>
       ) : (
-        <>
-          <h1 className="self-start text-white ml-[4vw] font-kiona text-2xl font-medium">
-            {" "}
-            Your Car
-          </h1>
-          <div className="w-[93%] h-fit grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 drop-shadow-md">
-            {isLoading ? (
-              <>
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-                <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
-              </>
-            ) : (
-              <div
-                onClick={(e) => {
-                  router.push("/add");
-                }}
-                className=" h-[35rem] m-2 rounded-lg relative hover:scale-[102%] transition duration-200 ease-in-out active:scale-100 flex-grow bg-zinc-900 flex flex-col justify-center items-center"
-              >
-                <h1 className="text-2xl text-white">You don't have any car.</h1>
-                <h1 className=" font-kiona text-white">
-                  Click here to add one
-                </h1>
-              </div>
-            )}
-          </div>
-          <h1 className="self-start text-white ml-[4vw] font-kiona pt-8 pb-2 text-2xl font-medium">
-            {" "}
-            Other Cars
-          </h1>
-        </>
+        session?.user.role === "carProvider" &&
+        myCar?.length! === 0 && (
+          <>
+            <h1 className="self-start text-white ml-[4vw] font-kiona text-2xl font-medium">
+              {" "}
+              Your Car
+            </h1>
+            <div className="w-[93%] h-fit grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 drop-shadow-md">
+              {isLoading ? (
+                <>
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                  <Skeleton className=" h-[35rem] m-2 rounded-lg bg-zinc-700 flex-grow" />
+                </>
+              ) : (
+                <div
+                  onClick={(e) => {
+                    router.push("/add");
+                  }}
+                  className=" h-[35rem] m-2 rounded-lg relative hover:scale-[102%] transition duration-200 ease-in-out active:scale-100 flex-grow bg-zinc-900 flex flex-col justify-center items-center"
+                >
+                  <h1 className="text-2xl text-white">
+                    You don't have any car.
+                  </h1>
+                  <h1 className=" font-kiona text-white">
+                    Click here to add one
+                  </h1>
+                </div>
+              )}
+            </div>
+            <h1 className="self-start text-white ml-[4vw] font-kiona pt-8 pb-2 text-2xl font-medium">
+              {" "}
+              Other Cars
+            </h1>
+          </>
+        )
       )}
 
       <div className="w-[93%] h-fit grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 drop-shadow-md">
