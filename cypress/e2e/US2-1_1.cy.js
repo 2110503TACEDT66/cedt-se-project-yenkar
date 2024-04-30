55e3bce7a3a68c49ff8ecaba8fc8f83661df7548
Nithinan_ <nwattanasiri47@gmail.com>
2024-04-30 23:49:57 +0700

    Added US2-1_2

--- cypress/e2e/US2-1_1.cy.js
+++ cypress/e2e/US2-1_1.cy.js
@@ -1,6 +1,6 @@
-describe('EPIC 2', () => {
-  describe('US2-1', () => {
-    it('Should be listed and visible (2.1.1 Valid)', () => {
+describe('US2-1_1', () => {
+  // describe('US2-1', () => {
+    beforeEach( () => {
       cy.visit(`http://localhost:3000/sign-in`)
       cy.get('#emailInput').as("email")
         .click()
@@ -11,6 +11,10 @@ describe('EPIC 2', () => {
       cy.get('Button[type="submit"]').click();
       cy.wait(2000);
       cy.visit(`http://localhost:3000/mystore`);
+    });
+
+    it('1st Car Should be listed and visible (2.1.1 Valid)', () => {
+      
       cy.wait(1500);
       // cy.get('#addCar').click();
       cy.visit(`http://localhost:3000/add`);
@@ -49,8 +53,52 @@ describe('EPIC 2', () => {
 
       cy.visit('http://localhost:3000/explore');
       expect('@newCar').to.exist;
-    })
+    });
+
+    it('2nd Car Should be listed and visible (2.1.1 Valid)', () => {
+      
+      cy.wait(1500);
+      // cy.get('#addCar').click();
+      cy.visit(`http://localhost:3000/add`);
+      cy.get('#model')
+        .click()
+        .type("civic")
+      cy.get('#brand')
+        .click()
+        .type("h");
+      cy.get('#plate')
+        .click()
+        .type('ab-1234');
+      cy.get('#price')
+        .click()
+        .type('500');
+      cy.get('#door')
+        .click()
+        .type('4');
+      cy.get('#seat')
+        .click()
+        .type('4');
+      cy.get('button').contains('Select your car transmission type').click();
+      cy.get('input[placeholder="Search"]')
+        .click()
+        .type('auto')
+        .type('{enter}');
+      cy.get('button').contains('Select your car cargo size').click();
+      cy.get('input[placeholder="Search"]')
+        .click()
+        .type('small')
+        .type('{enter}');
+      cy.get('#submit').click();
+      cy.visit('http://localhost:3000/mystore');
+      cy.get('h1').contains('honda').as('newCar');
+      expect('@newCar').to.exist;
+
+      cy.visit('http://localhost:3000/explore');
+      expect('@newCar').to.exist;
+    });
+
     
-  })
+    
+  // })
   
 })
